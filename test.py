87462b1d8cb59fbe3c10d8d9003d6458ed2b5c43
shi.fan <shifan3rd@gmail.com>
2023-06-23 10:06:10 +0800

    ad

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 import json
-resp = requests.post('http://localhost:8080/completion', data = {
+resp = requests.post('http://localhost:8080/completion', params = {
     'prompt' : '生物:根生长具有向 _ _生长、向 _ _生长和向 _ _生长的特性．\n知识点:',
     'n_predict' : 32,
     'stop' : ['END']
