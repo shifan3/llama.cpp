7b1ae2d0d1622db91197e3e56b66d8b7c8ec12f2
shi.fan <shifan3rd@gmail.com>
2023-06-23 10:12:02 +0800

    qweq

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ t1 = time()
 resp = requests.post('http://localhost:8080/completion', data = json.dumps({
     'prompt' : '生物:根生长具有向 _ _生长、向 _ _生长和向 _ _生长的特性．\n知识点:',
     'n_predict' : 32,
-    'stop' : ['END']
+    'stop' : ['END'],
+    'top_k' : 3,
 }, ensure_ascii=False).encode('utf-8'))
 
 print(json.dumps(json.loads(resp.text), indent = 4, ensure_ascii=False))
