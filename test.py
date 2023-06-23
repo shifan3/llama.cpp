a1eb65c758694228350c3635c7ed30929c9e2c03
shi.fan <shifan3rd@gmail.com>
2023-06-23 15:01:20 +0800

    ada

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import requests
 import json
 from time import time
 t1 = time()
-prompt = '（2014秋•益阳校级期末）现有常温时浓度相同的五种电解质溶液：①CH3COOH ②Na2CO3 ③HNO3④CH3COONa ⑤NaOH （1）五种溶质中是弱电解质的是 _ _（填编号）； （2）④溶液中离子与分子共 _ _种（包括水分子）； （3）这五种溶液的pH由小到大的顺序是 _ _（填编号）； （4）将CH3COONa溶液稀释100倍时，其pH变化如图中 _ _曲线（填字母）． （5）上'
+prompt = '一个自然数（0除外）， _ _的个数是有限的， _ _的个数是无限的．'
 partial_knowledge = ''
 n_predict = 32
 while True:
     resp = requests.post('http://localhost:8080/completion', data = json.dumps({
         'prompt' : f'{prompt}\n知识点:\n{partial_knowledge}',
-        'subject' : '化学',
+        'subject' : '数学',
         'n_predict' : n_predict,
         'stop' : ['END'],
     }, ensure_ascii=False).encode('utf-8'))
